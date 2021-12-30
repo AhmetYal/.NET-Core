8a5eb8170d8577ca6caf2b1a18a2173a49bdc7a0
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2021-12-30 16:20:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _NET_Core
+{
+    class Program
+    {
+        private const string V = "merhaba";
+
+        public  static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+             Console.WriteLine("İsmini gir");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyisim");
+            string surname=Console.ReadLine();
+Console.WriteLine(V + name+""+surname);
+        }
+    }
+}
